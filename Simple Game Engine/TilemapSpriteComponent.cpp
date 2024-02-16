75ad49d0107185b63ceb21358b3bdf060f49305c
ArkanioGX <clarencemollet1@gmail.com>
2024-02-16 10:06:00 +0100

    Remove log

--- Simple Game Engine/TilemapSpriteComponent.cpp
+++ Simple Game Engine/TilemapSpriteComponent.cpp
@@ -1,5 +1,5 @@
 #include "TilemapSpriteComponent.h"
-#include "Log.h"
+
 
 TilemapSpriteComponent::TilemapSpriteComponent(Actor* ownerP, Tilemap* tmap, int drawOrderP):
 	SpriteComponent(ownerP, tmap->getTexture(), drawOrderP),
@@ -16,7 +16,7 @@ void TilemapSpriteComponent::draw(Renderer& renderer)
 {
 	Vector2 max = map->getMaxTile();
 	
-	Log::info(std::to_string(max.x));
+	
 
 	int tileNumberH = max.y;
 	int tileNumberW = max.x;
